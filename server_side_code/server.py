dd0489ed6ccb671ebacf226b46d3299a1d52940a
Chris Paulson <capaulson@gmail.com>
2012-01-02 18:54:47 -0800

    switched to blowfish, for a variable length encryption key

--- server_side_code/server.py
+++ server_side_code/server.py
@@ -8,11 +8,11 @@ Created on Mon Jan  2 15:52:38 2012
 ## This is a simple server for encryption protocol testing, it's built on twisted. Currently, it'll echo any tcp data recieved on port 1234
 
 from twisted.internet import protocol, reactor
-from Crypto.Cipher import AES
+from Crypto.Cipher import Blowfish
 
 class Echo(protocol.Protocol):
     def dataReceived(self,data):
-            encdec = encrypt_decrypt('This is a key')
+            encdec = encrypt_decrypt('This is a key gsdgsgdfggd')
             print 'Recieved: ',data
             sent_data = encdec.text_encrypt(data)
             print 'Returned: ',sent_data 
@@ -25,9 +25,11 @@ class EchoFactory(protocol.Factory):
             
 class encrypt_decrypt():
     def __init__(self, key):
-        self.obj = AES.new(str('this is a key'), AES.MODE_ECB)
+        self.obj = Blowfish.new(key, Blowfish.MODE_ECB)
         
     def text_encrypt(self,message):
+        while len(message)%8!=0:
+            message += ' '
         return self.obj.encrypt(message)
     
     def text_decrypt(self,encmsg):
