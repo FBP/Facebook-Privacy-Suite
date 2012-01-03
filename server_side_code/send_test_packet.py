6843273c234a6a5c9d0cd8f661a04f876eba2087
Chris Paulson <capaulson@gmail.com>
2012-01-02 21:51:19 -0800

    setup encrypt and decrypt server

--- server_side_code/send_test_packet.py
+++ server_side_code/send_test_packet.py
@@ -7,11 +7,20 @@ Created on Mon Jan  2 16:37:54 2012
 
 import socket
 
-host = 'localhost'
-port = 1234
+host = 'fbp.servebeer.com'
+port_encrypt = 1234
+port_decrypt = 1235
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host,port))
-s.send('Hello World!')
+s.connect((host,port_encrypt))
+s.send('Vera is SUPER hot!!!')
 
-print(s.recv(1024))
+recieved = s.recv(1024)
+print 'Encrypted: ', recieved
+
+##
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s1.connect((host,port_decrypt))
+s1.send(recieved)
+
+print 'Decrypted: ', s1.recv(1024)
--- server_side_code/server.py
+++ server_side_code/server.py
@@ -9,19 +9,33 @@ Created on Mon Jan  2 15:52:38 2012
 
 from twisted.internet import protocol, reactor
 from Crypto.Cipher import Blowfish
+from multiprocessing import Process
+import binascii
 
-class Echo(protocol.Protocol):
+class EncryptEcho(protocol.Protocol):
     def dataReceived(self,data):
             encdec = encrypt_decrypt('This is a key gsdgsgdfggd')
             print 'Recieved: ',data
             sent_data = encdec.text_encrypt(data)
-            print 'Returned: ',sent_data 
+            print 'Returned: ', sent_data 
             self.transport.write(sent_data)
-            print 'Returned Decrypted: ', encdec.text_decrypt(sent_data)
  
-class EchoFactory(protocol.Factory):
+class EncryptFactory(protocol.Factory):
     def buildProtocol(self,addr):
-            return Echo()
+            return EncryptEcho()
+            
+            
+class DecryptEcho(protocol.Protocol):
+    def dataReceived(self,data):
+            encdec = encrypt_decrypt('This is a key gsdgsgdfggd')
+            print 'Recieved: ',data
+            sent_data = encdec.text_decrypt(data)
+            print 'Returned: ', sent_data 
+            self.transport.write(sent_data)
+ 
+class DecryptFactory(protocol.Factory):
+    def buildProtocol(self,addr):
+            return DecryptEcho()
             
 class encrypt_decrypt():
     def __init__(self, key):
@@ -30,13 +44,14 @@ class encrypt_decrypt():
     def text_encrypt(self,message):
         while len(message)%8!=0:
             message += ' '
-        return self.obj.encrypt(message)
+        return binascii.b2a_base64(self.obj.encrypt(message))
     
     def text_decrypt(self,encmsg):
-        return self.obj.decrypt(encmsg)
+        return self.obj.decrypt(binascii.a2b_base64(encmsg))
     
     
     
- 
-reactor.listenTCP(1234,EchoFactory())
-reactor.run()
+if __name__ == '__main__': 
+    reactor.listenTCP(1234,EncryptFactory())
+    reactor.listenTCP(1235,DecryptFactory())
+    reactor.run()
