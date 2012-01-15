44cd2162cb2daaf756f2106b1c19712afbfdc3e5
Chris Paulson <capaulson@gmail.com>
2012-01-14 23:14:19 -0800

    fs cleaning

---
+++ server/http/webserver.py
@@ -0,0 +1,71 @@
+#Copyright Jon Berg , turtlemeat.com
+
+import string,cgi,time
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from M2Crypto import RSA
+from base64 import b64encode
+
+#import pri
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print 'Generating RSA Key'
+        key = RSA.gen_key(1024, 65337)
+        raw_key = key.pub()[1]
+        self.b64key = b64encode(raw_key)
+        print self.b64key
+        try:
+            if self.path.endswith(".html"):
+                f = open(curdir + sep + self.path) #self.path has /test.html
+#note that this potentially makes every file on your computer readable by the internet
+
+                self.send_response(200)
+                self.send_header('Content-type',	'text/html')
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+                return
+            if self.path.endswith("getpub.esp"):   #our dynamic content
+                self.send_response(200)
+                self.send_header('Content-type',	'text/html')
+                self.end_headers()
+                self.wfile.write(self.b64key)
+                return
+                
+            return
+                
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+     
+
+    def do_POST(self):
+        global rootnode
+        try:
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                query=cgi.parse_multipart(self.rfile, pdict)
+            self.send_response(301)
+            
+            self.end_headers()
+            upfilecontent = query.get('upfile')
+            print "filecontent", upfilecontent[0]
+            self.wfile.write("<HTML>POST OK.<BR><BR>");
+            self.wfile.write(upfilecontent[0]);
+            
+        except :
+            pass
+
+def main():
+    try:
+        server = HTTPServer(('', 80), MyHandler)
+        print 'started httpserver...'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
+
---
+++ server/tcp/send_test_packet.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  2 16:37:54 2012
+
+@author: - Chris Paulson
+"""
+
+import socket
+
+host = 'fbp.servebeer.com'
+port_encrypt = 1234
+port_decrypt = 1235
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host,port_encrypt))
+s.send('Test Message Here')
+
+recieved = s.recv(1024)
+print 'Encrypted: ', recieved
+
+##
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s1.connect((host,port_decrypt))
+s1.send(recieved)
+
+print 'Decrypted: ', s1.recv(1024)
